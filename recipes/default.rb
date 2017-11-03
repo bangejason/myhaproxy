43724e1299b60640565b95a56fec5dc1ef8cf43d
bangejason <jbange79@gmail.com>
2017-11-03 14:30:27 +0000

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ec2-34-229-185-143.compute-1.amazonaws.com',
+    'ipaddress' => '172.31.29.144',
+    'port' => 80,
+    'ssl_port' => 80
+  }]
+
+include_recipe 'haproxy::manual'
