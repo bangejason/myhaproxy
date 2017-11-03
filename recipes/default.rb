8dfb11444f7d619b0122ebc930ba4f4db78bf777
bangejason <jbange79@gmail.com>
2017-11-03 18:59:32 +0000

    Version 1.0.0, dynamically update load balancing pool

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,18 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+all_web_nodes = search('node','role:web_server')
+members = []
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' => 80
+  }
+  members.push(member)
+end
+
 node.default['haproxy']['members'] = [{
     'hostname' => 'ec2-34-229-185-143.compute-1.amazonaws.com',
     'ipaddress' => '172.31.29.144',
